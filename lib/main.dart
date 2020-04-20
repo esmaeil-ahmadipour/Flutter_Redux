ac3c7ad820f0388c7d002651e49c502f76751c59
Esmaeil Ahmadipour <software8686@gmail.com>
2020-04-20 15:50:43 +0430

    commit> Set Configurations Of Main According Redux-Pattern (Part1)

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,39 @@
 import 'package:flutter/material.dart';
-import 'package:flutterredux/home.dart';
 import 'package:flutterredux/about.dart';
+import 'package:flutterredux/home.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:flutterredux/models/app_state.dart';
+import 'package:flutterredux/redux/reducers.dart';
 import 'package:flutterredux/settings.dart';
+import 'package:redux/redux.dart';
+import 'package:flutterredux/redux/reducers.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  final _initialState = AppState(sliderFontSize: 0.5);
+  final Store<AppState> _store =
+      Store<AppState>(reducer, initialState: _initialState);
+  runApp(MyApp(
+    store: _store,
+  ));
+}
 
 class MyApp extends StatelessWidget {
+  final Store<AppState> store;
+
+  MyApp({this.store});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      initialRoute: '/',
-      routes: {
-        '/': (context) => Home(),
-        '/about': (context) => About(),
-        '/settings': (context) => Settings(),
-      },
+    return StoreProvider<AppState>(
+      child: MaterialApp(
+        initialRoute: '/',
+        routes: {
+          '/': (context) => Home(),
+          '/about': (context) => About(),
+          '/settings': (context) => Settings(),
+        },
+      ),
+      store: store,
     );
   }
 }
